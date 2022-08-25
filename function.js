e16507de478540e5d0384141e444e69dd3ea686b
saikab <97691842+saikab@users.noreply.github.com>
2022-08-26 00:27:21 +0530

    function assignment 2 for relevel

---
+++ function.js
@@ -0,0 +1,36 @@
+
+function largerNumber (a,b){
+
+    if(a>b) {
+     
+     
+      console.log("Larger number is="+a);
+    
+    
+    }
+
+    else if(b>a) {
+      
+      
+      console.log("Larger number is="+b);
+    
+    }
+
+    else if (a=b) {
+
+      console.log("Both number is equal");
+
+    }
+
+    else {
+      console.log("Wrong Input")
+    }
+
+
+}
+
+
+largerNumber(9999,9999);
+
+
+
